1223cef7e9f8de8996fe6bf477babb36c534fca3
noobimpostor2 <102277316+noobimpostor2@users.noreply.github.com>
2023-02-25 21:14:13 +0530

    Ok

--- remocolab.py
+++ remocolab.py
@@ -102,7 +102,7 @@ def _check_gpu_available():
   else:
     return True
 
-  return IPython.utils.io.ask_yes_no("Do you want to continue? [y/n]")
+  return True
 
 def _set_public_key(user, public_key):
   if public_key != None:
